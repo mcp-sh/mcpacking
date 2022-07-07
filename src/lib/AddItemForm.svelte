f2cc7eb7785049147b385e6a7bfe33a969551fb6
Office - MCP <mcp@macapa.me>
2022-07-07 15:14:06 +0800

    Submit Form by return

--- src/lib/AddItemForm.svelte
+++ src/lib/AddItemForm.svelte
@@ -21,15 +21,17 @@
 <h1 class="text-center font-extralight text-2xl text-blue-600 mb-4">Mehr Zeug</h1>
 <div class="add-item-form mb-4">
 	<div class="form-control ">
-		<label class="input-group">
-			<span>Title</span>
-			<input
-				bind:value={newItemTitle}
-				type="text"
-				placeholder="Zeug"
-				class="input input-bordered w-full"
-			/>
-		</label>
-		<button on:click={addItem} class="btn btn-primary text-primary-content mt-2">Hinzufügen</button>
+		<form on:submit|preventDefault="{addItem}">
+			<label class="input-group">
+				<span>Title</span>
+				<input
+					bind:value={newItemTitle}
+					type="text"
+					placeholder="Zeug"
+					class="input input-bordered w-full"
+				/>
+			</label>
+			<button class="btn btn-primary text-primary-content mt-2">Hinzufügen</button>
+		</form>
 	</div>
 </div>
